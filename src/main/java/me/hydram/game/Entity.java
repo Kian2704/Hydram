13e669ed5129f0bffb393c1a90f54d6fdc5b3358
Knork <77513039+JonasS-de@users.noreply.github.com>
2023-05-23 19:29:03 +0200

    Comments and minor refraktor for Entity.java

--- src/main/java/me/hydram/game/Entity.java
+++ src/main/java/me/hydram/game/Entity.java
@@ -2,18 +2,16 @@ package me.hydram.game;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/*Wesen grundobjekt*/
+
 public class Entity extends ImageView {
 	
 	protected Image texture;
-	protected int type;//0 Player(Pacman), 1 Ghost/NPC,3 Point, 4 Powerup,5 Spawn Blocker
+	protected int type;		//Bestimmt den Typen des Objekts 0 Player(Pacman), 1 Ghost/NPC,3 Point, 4 Powerup,5 Spawn Blocker
 	protected double height;
 	protected double width;
 	protected double sizeFactor = 0.9;
 	
-	
-	
-	protected Entity()
-	{
-	}
+	protected Entity() {}
 
 }
