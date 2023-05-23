8e486490475041bf475348053a7f5b7a6773c34b
Knork <77513039+JonasS-de@users.noreply.github.com>
2023-05-23 19:06:27 +0200

    Commenting and minor refractoring of Debug.java

--- src/main/java/me/hydram/game/Debug.java
+++ src/main/java/me/hydram/game/Debug.java
@@ -3,54 +3,48 @@ package me.hydram.game;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
 
-
+/*Diverse Funktionen welche bei der Käferbehebung hilfreich sind*/
 
 public class Debug extends Game {
 
-	public static boolean debugMode = false;
-	
-	public static void tileDebugEventHandler(Tile tile)
-	{
-		tile.setOnMouseClicked(new EventHandler<MouseEvent>(){
+    public static boolean debugMode = false;
 
-			@Override
-			public void handle(MouseEvent arg0) {
-				System.out.println("Tile Coordinates: " + tile.col + " " + tile.row);	
-				System.out.println("Tile Type: " + tile.type);
-				if(tile.getEnt() != null)
-				{
-					System.out.println("Entity Type: " + tile.getEnt().type);
-					System.out.println("Entity Size: " +tile.getEnt().sizeFactor);
-				}
-			}
-		});
-	}	
-	
-	
-	
-	
-	public static void entityDebugEventHandler(Entity ent)
-	{
-		ent.setOnMouseClicked(new EventHandler<MouseEvent>(){
+    public static void tileDebugEventHandler(Tile tile) {
+        tile.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
-			@Override
-			public void handle(MouseEvent arg0) {
-				System.out.println("Entity Coordinates: " + ent.getLayoutX() + " " + ent.getLayoutY());	
-				System.out.println("Entity Type: " + ent.type);
-			}
-		});
-	}	
-	public static void printPacmans()
-	{
-		gameScene.setOnKeyPressed(e -> {
-		    if (e.getCode() == KeyCode.A) {
-		        System.out.println(Main.currentGame.getPacman().getLayoutX());
-		        System.out.println(Main.currentGame.getPacman2().getLayoutX());
-		        
-		    }
-		});
-	}
+            @Override
+            public void handle(MouseEvent arg0) {
+                System.out.println("Tile Coordinates: " + tile.col + " " + tile.row);
+                System.out.println("Tile Type: " + tile.type);
+                if (tile.getEnt() != null) {
+                    System.out.println("Entity Type: " + tile.getEnt().type);
+                    System.out.println("Entity Size: " + tile.getEnt().sizeFactor);
+                }
+            }
+        });
+    }
+
+
+    public static void entityDebugEventHandler(Entity ent) {
+        ent.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent arg0) {
+                System.out.println("Entity Coordinates: " + ent.getLayoutX() + " " + ent.getLayoutY());
+                System.out.println("Entity Type: " + ent.type);
+            }
+        });
+    }
+
+    public static void printPacmans() {
+        gameScene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.A) {
+                System.out.println(Main.currentGame.getPacman().getLayoutX());
+                System.out.println(Main.currentGame.getPacman2().getLayoutX());
+
+            }
+        });
+    }
 
 }
